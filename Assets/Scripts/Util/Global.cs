0498b0da3ae5d772aa28683e8a4a30211874655d
Isaiah Mann <mannisai@amazon.com>
2017-12-26 18:39:29 -0500

    Add SFX

--- Assets/Scripts/Util/Global.cs
+++ Assets/Scripts/Util/Global.cs
@@ -5,6 +5,7 @@
 	public const string LEFT_PLAYER_GOAL = LEFT_PLAYER + GOAL;
 	public const string RIGHT_PLAYER_GOAL = RIGHT_PLAYER + GOAL;
 	public const string PUCK = "Puck";
+	public const string BRICK = "Brick";
 
 	public const float BASE_PADDLE_SPEED = 15.0f;
 	public const float MAX_PUCK_STARTING_SPEED = 15.5f;
