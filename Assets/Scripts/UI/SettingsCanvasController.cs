7e330fa5a1b62b131fc37a807ec387bc814578d8
Isaiah Mann <mannisai@amazon.com>
2017-12-28 16:52:43 -0500

    Add audio toggle buttons

---
+++ Assets/Scripts/UI/SettingsCanvasController.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Settings screen
+ */
+
+using UnityEngine;
+
+public class SettingsCanvasController : UIController
+{
+	const string ON = "On";
+	const string OFF = "Off";
+	const string MUSIC = "Music";
+	const string SFX = "Sound Effects";
+
+	[SerializeField]
+	UIButton musicButton;
+	[SerializeField]
+	UIButton sfxButton;
+
+	protected override void Start()
+	{
+		base.Start();
+		musicButton.AddAction(toggleMusic);
+		musicButton.AddAction(setButtons);
+		sfxButton.AddAction(toggleSFX);
+		sfxButton.AddAction(setButtons);
+		setButtons();
+	}
+
+	void setButtons()
+	{
+		musicButton.SetText(getMusicButtonText());
+		sfxButton.SetText(getSFXButtonText());
+	}
+
+	string getMusicButtonText()
+	{
+		return string.Format("{0}: <i>{1}</i>", MUSIC, SettingsUtil.MusicMuted ? OFF : ON);
+	}
+
+	string getSFXButtonText()
+	{
+		return string.Format("{0}: <i>{1}</i>", SFX, SettingsUtil.SFXMuted ? OFF : ON);
+	}
+		
+	void toggleMusic()
+	{
+		SettingsUtil.ToggleMusicMuted();
+	}
+
+	void toggleSFX()
+	{
+		SettingsUtil.ToggleSFXMuted();	
+	}
+}
